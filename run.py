36a680a058275334cffc7ceef76bc13e17bdc973
LewisCM14 <Lewiscmartin@live.co.uk>
2021-10-10 12:47:44 +0000

    add letters_to_numbers to GameBoard class

--- run.py
+++ run.py
@@ -41,6 +41,13 @@ class GameBoard:
         self.board = board
         self.name = name
         self.score = score
+    
+    """def get_letters_to_numbers():
+       
+        Converts the letters used for display to numbers for function
+       
+        letters_to_numbers = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7, 'I': 8, 'J': 9}
+        return letters_to_numbers"""
 
     def print_board(self):
         """
